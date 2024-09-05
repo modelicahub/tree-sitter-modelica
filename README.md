502e5c8c7c768984a0c373f10e91203327ef82c5
Mohamad Omar Nachawati <mnachawa@gmail.com>
2024-09-05 11:38:53 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# tree-sitter-modelica
+
+Modelica grammar for [tree-sitter](https://github.com/tree-sitter/tree-sitter).
---
+++ bindings/python/tests/test_binding.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+import tree_sitter, tree_sitter_modelica
+
+
+class TestLanguage(TestCase):
+    def test_can_load_grammar(self):
+        try:
+            tree_sitter.Language(tree_sitter_modelica.language())
+        except Exception:
+            self.fail("Error loading Modelica grammar")
